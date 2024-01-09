946e0678304e3adce1b8dcf1a9c718829377a3fb
AndoreHtet <hhtetaung578@gmail.com>
2024-01-09 10:06:37 +0630

    filled with data

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -22,7 +23,7 @@ public class BookShopApplication {
     private final BookDao bookDao;
     private final GenreDao genreDao;
     private final PublisherDao publisherDao;
-    @Bean
+    @Bean @Transactional
     public ApplicationRunner runner(){
         return r ->{
             Author author1 = new Author("Charles Dickens","charles@gmail.com");
@@ -44,6 +45,87 @@ public class BookShopApplication {
                     20,
                     "https://source.unsplash.com/400x300/?flower"
             );
+
+            Book book2 = new Book(
+                    2, IsbnGenerator.generate(),
+                    "Great Expectations",
+                    "Good Choice",
+                    25.3,
+                    20,
+                    "https://source.unsplash.com/400x300/?nature"
+            );
+            Book book3 = new Book(
+                    3, IsbnGenerator.generate(),
+                    "Bleak House",
+                    "Nice",
+                    22.3,
+                    20,
+                    "https://source.unsplash.com/400x300/?ocean"
+            );
+            Book book4 = new Book(
+                    4, IsbnGenerator.generate(),
+                    "Under The Greenwood Tree",
+                    "Excellent",
+                    50.3,
+                    20,
+                    "https://source.unsplash.com/400x300/?flower"
+            );
+            Book book5 = new Book(
+                    5, IsbnGenerator.generate(),
+                    "Return of the Native",
+                    "Excellent",
+                    25.3,
+                    20,
+                    "https://source.unsplash.com/400x300/?flower"
+            );
+            Book book6 = new Book(
+                    6, IsbnGenerator.generate(),
+                    "Far from the Madding Crowd",
+                    "Excellent",
+                    50.3,
+                    20,
+                    "https://source.unsplash.com/400x300/?flower"
+            );
+            //mapping
+            author1.addBook(book1);
+            author1.addBook(book2);
+            author1.addBook(book3);
+            author2.addBook(book4);
+            author2.addBook(book5);
+            author2.addBook(book6);
+
+            Publisher pub1 = publisherDao.save(publisher1);
+            pub1.addBook(book1);
+            pub1.addBook(book2);
+            pub1.addBook(book3);
+
+            Publisher pub2 = publisherDao.save(publisher2);
+            pub2.addBook(book4);
+            pub2.addBook(book5);
+            pub2.addBook(book6);
+
+            Genre gen1 = genreDao.save(genre1);
+            Genre gen2 = genreDao.save(genre2);
+            book1.addGenres(gen1);
+            book2.addGenres(gen1);
+            book3.addGenres(gen1);
+
+            book4.addGenres(gen2);
+            book5.addGenres(gen2);
+            book6.addGenres(gen2);
+
+//            bookDao.save(book1);
+//            bookDao.save(book2);
+//            bookDao.save(book3);
+//            bookDao.save(book4);
+//            bookDao.save(book5);
+//            bookDao.save(book6);
+
+            authorDao.save(author1);
+            authorDao.save(author2);
+
+
+
 
         };
     }
