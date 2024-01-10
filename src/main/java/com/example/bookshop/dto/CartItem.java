9b15bc33af9d5570ebe5ac9524ec692ef72d2248
AndoreHtet <hhtetaung578@gmail.com>
2024-01-10 14:02:37 +0630

    cart add

--- src/main/java/com/example/bookshop/dto/CartItem.java
+++ src/main/java/com/example/bookshop/dto/CartItem.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,4 +16,17 @@ public class CartItem {
     private String title;
     private double price;
     private int quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return id == cartItem.id && Objects.equals(isbn, cartItem.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn);
+    }
 }
